48a5a951065a6f3efc31a98a6776420f5551474d
Phuong Le <phuonganh.le0312@gmail.com>
2020-08-15 15:41:34 -0400

    deployed to heroku

--- server.js
+++ server.js
@@ -2,10 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const path = require("path");
-
-
 const PORT = process.env.PORT || 3000;
-
 const app = express();
 
 app.use(logger("dev"));
