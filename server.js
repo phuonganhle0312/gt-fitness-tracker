c2b450a8f40233d326ed844ea5187ee01631d5e7
Phuong Le <phuonganh.le0312@gmail.com>
2020-08-15 15:47:11 -0400

    fixing heroku error

--- server.js
+++ server.js
@@ -2,8 +2,15 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const path = require("path");
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 const app = express();
+
+app.set( 'port', ( process.env.PORT || 3000 ));
+
+// Start node server
+app.listen( app.get( 'port' ), function() {
+  console.log( 'Node server is running on port ' + app.get( 'port' ));
+  });
 
 app.use(logger("dev"));
 
@@ -28,6 +35,6 @@ app.get("/stats", (req, res) => {
     res.sendFile(path.join(__dirname + "/public/stats.html"));
 });
 
-app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
-});
+// app.listen(PORT, () => {
+//     console.log(`App running on port ${PORT}!`);
+// });
