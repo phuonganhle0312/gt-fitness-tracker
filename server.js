4ba3d3ca9fa1b18ed41acd2c01c1bb7740efaeb4
Phuong Le <phuonganh.le0312@gmail.com>
2020-08-14 21:20:09 -0400

    added server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const viewRoutes = require("./routes/viewRoutes.js");
+const apiRoutes = require("./routes/apiRoutes.js")
+
+const PORT = process.env.PORT || 3030;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+app.use(viewRoutes);
+app.use(apiRoutes);
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+  });
